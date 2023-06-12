5f4e1f37d3463ec98f1049dc861be7efa06f0537
IMU <IMU@DESKTOP-FSRQE0S>
2023-06-12 08:53:18 +0630

    Versioin 2

---
+++ src/pac1/Student.java
@@ -0,0 +1,34 @@
+package pac1;
+
+public class Student {
+	private String name;
+	private int age;
+	private String phno;
+	private String address;
+
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public int getAge() {
+		return age;
+	}
+	public void setAge(int age) {
+		this.age = age;
+	}
+	public String getPhno() {
+		return phno;
+	}
+	public void setPhno(String phno) {
+		this.phno = phno;
+	}
+	public String getAddress() {
+		return address;
+	}
+	public void setAddress(String address) {
+		this.address = address;
+	}
+	
+}
